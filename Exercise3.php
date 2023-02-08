74bdfe7e05d4eb4d3e2febdd5821768a121f5142
MichaelYaps <michaelakichi@gmail.com>
2023-02-08 21:43:57 +0000

    Update exercise

--- Exercise3.php
+++ Exercise3.php
@@ -2,18 +2,19 @@
 
 // Valid Integer.
 function myFunction($nb) {
-    if(is_int($nb)) {
-        echo "$nb is a valid integer.";
-    } else {
-        echo "$nb is not a valid integer.";
+
+    try {
+        // verif is integer 
+        if (is_int($nb)) {
+            echo "$nb is a valid integer.";
+        } else {
+            throw new Exception("$nb is not a valid integer.", 1);
+        }
+    } catch (Exception $ex) {
+        echo $ex->getMessage();
+    } finally {
+        echo "Validation Complete";
     }
 }
 
-try {
-    // verif is integer 
-    myFunction(2.5);
-} catch (Exception $ex) {
-    echo $ex->getMessage();
-} finally {
-    echo "<br/> Validation Complete";
-}
+myFunction(2.5);
